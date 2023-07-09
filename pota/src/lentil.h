da8cdac2f3659b82cc7494cc2f2c6c7439a8cbce
Brian R Hanke <brianrhanke@outlook.com>
2023-07-09 10:38:33 -0400

    Update lentil.h

--- pota/src/lentil.h
+++ pota/src/lentil.h
@@ -32,18 +32,21 @@ extern bool l_critsec_active;
 inline bool lentil_crit_sec_init() {
     // Called in node_plugin_initialize. Returns true as a convenience.
     l_critsec_active = true;
+    //std::lock_guard<AtMutex> guard(l_critsec);
     return true;
 }
 
 inline void lentil_crit_sec_close() {
     // Called in node_plugin_cleanup
     l_critsec_active = false;
+    //std::lock_guard<AtMutex> guard(l_critsec);
 }
 
 inline void lentil_crit_sec_enter() {
     // If the crit sec has not been inited since last close, we simply do not enter.
     // (Used by Cryptomatte filter.)
     if (l_critsec_active)
+        //std::lock_guard<AtMutex> guard(l_critsec);
         l_critsec.lock();
 }
 
@@ -51,6 +54,7 @@ inline void lentil_crit_sec_leave() {
     // If the crit sec has not been inited since last close, we simply do not enter.
     // (Used by Cryptomatte filter.)
     if (l_critsec_active)
+        //std::lock_guard<AtMutex> guard(l_critsec);
         l_critsec.unlock();
 }
 
@@ -221,7 +225,7 @@ public:
         // if (!(po->stored_useImage == AiNodeGetBool(node, "bokeh_enable_imagePO") && po->stored_path == AiNodeGetStr(node, "bokeh_image_pathPO")) {
         image.invalidate();
         if (bokeh_enable_image && !image.read(bokeh_image_path.c_str())){
-            AiMsgError("[LENTIL CAMERA PO] Couldn't open bokeh image!");
+            AiMsgError("[LENTIL CAMERA] Could not open bokeh image.");
             AtRenderSession *render_session = AiUniverseGetRenderSession(universe);
             AiRenderAbort(render_session);
         }
@@ -261,7 +265,7 @@ public:
 
                     if (time_cnt == 500) { // guess that crypto is in the same queue, behind lentil
                         crypto_in_same_queue = true;
-                        AiMsgInfo("[LENTIL] Waiting for Cryptomatte setup reached time-out.");
+                        AiMsgInfo("[LENTIL] Timed out waiting for cryptomatte setup.");
                         break;
                     }
                 }
@@ -1001,7 +1005,7 @@ public:
         AiNodeIteratorDestroy(iter);
 
         if (!lentil_operator_node) {
-            AiMsgError("[LENTIL] Since Lentil 2.5, lentil requires an operator (lentil_operator) to function. Please insert this operator.");
+            AiMsgError("[LENTIL] Missing operator: lentil_operator.");
         }
         
         OperatorData *operator_data = (OperatorData*)AiNodeGetLocalData(lentil_operator_node);
@@ -1115,7 +1119,7 @@ public:
                     aov.allocate_regular_buffers(xres, yres);
                 }
 
-                AiMsgInfo("[LENTIL BIDIRECTIONAL] Driver '%s' -- Adding aov %s of type %s", aov.to.driver_tok.c_str(), aov.to.aov_name_tok.c_str(), aov.to.aov_type_tok.c_str());
+                AiMsgInfo("[LENTIL BIDIRECTIONAL] Driver '%s' - Adding AOV %s of type %s", aov.to.driver_tok.c_str(), aov.to.aov_name_tok.c_str(), aov.to.aov_type_tok.c_str());
             }
         }
     }
@@ -1151,7 +1155,7 @@ private:
 
 
         if (!enable_dof) {
-            AiMsgWarning("[LENTIL BIDIRECTIONAL] Depth of field is disabled, therefore disabling bidirectional sampling.");
+            AiMsgWarning("[LENTIL BIDIRECTIONAL] Depth of field is disabled, disabling bidirectional sampling.");
             return false;
         }
 
@@ -1162,7 +1166,7 @@ private:
 
         // if progressive rendering is on, don't redistribute
         if (enable_dof && bidir_sample_mult != 0 && AiNodeGetBool(AiUniverseGetOptions(universe), AtString("enable_progressive_render"))) {
-            AiMsgError("[LENTIL BIDIRECTIONAL] Progressive rendering is not supported. Arnold does not yet provide enough API functionality for this to be implemented as it should.");
+            AiMsgError("[LENTIL BIDIRECTIONAL] Progressive rendering is not supported.");
             AtRenderSession *render_session = AiUniverseGetRenderSession(universe);
             AiRenderAbort(render_session);
             return false;
@@ -1576,29 +1580,29 @@ private:
                     #include "../include/auto_generated_lens_includes/load_lens_constants.h"
                 }
 
-                AiMsgInfo("[LENTIL CAMERA PO] ----------  LENS CONSTANTS  -----------");
-                AiMsgInfo("[LENTIL CAMERA PO] Lens Name: %s", lens_name);
-                AiMsgInfo("[LENTIL CAMERA PO] Lens F-Stop: %f", lens_fstop);
+                AiMsgInfo("[LENTIL CAMERA] ----------  LENS CONSTANTS  -----------");
+                AiMsgInfo("[LENTIL CAMERA] Lens name: %s", lens_name);
+                AiMsgInfo("[LENTIL CAMERA] Lens f-stop: %f", lens_fstop);
                 #ifdef DEBUG_LOG
-                AiMsgInfo("[LENTIL CAMERA PO] lens_outer_pupil_radius: %f", lens_outer_pupil_radius);
-                AiMsgInfo("[LENTIL CAMERA PO] lens_inner_pupil_radius: %f", lens_inner_pupil_radius);
-                AiMsgInfo("[LENTIL CAMERA PO] lens_length: %f", lens_length);
-                AiMsgInfo("[LENTIL CAMERA PO] lens_back_focal_length: %f", lens_back_focal_length);
-                AiMsgInfo("[LENTIL CAMERA PO] lens_effective_focal_length: %f", lens_effective_focal_length);
-                AiMsgInfo("[LENTIL CAMERA PO] lens_aperture_pos: %f", lens_aperture_pos);
-                AiMsgInfo("[LENTIL CAMERA PO] lens_aperture_housing_radius: %f", lens_aperture_housing_radius);
-                AiMsgInfo("[LENTIL CAMERA PO] lens_inner_pupil_curvature_radius: %f", lens_inner_pupil_curvature_radius);
-                AiMsgInfo("[LENTIL CAMERA PO] lens_outer_pupil_curvature_radius: %f", lens_outer_pupil_curvature_radius);
-                AiMsgInfo("[LENTIL CAMERA PO] lens_inner_pupil_geometry: %s", lens_inner_pupil_geometry.c_str());
-                AiMsgInfo("[LENTIL CAMERA PO] lens_outer_pupil_geometry: %s", lens_outer_pupil_geometry.c_str());
-                AiMsgInfo("[LENTIL CAMERA PO] lens_field_of_view: %f", lens_field_of_view);
-                AiMsgInfo("[LENTIL CAMERA PO] lens_aperture_radius_at_fstop: %f", lens_aperture_radius_at_fstop);
+                AiMsgInfo("[LENTIL CAMERA] lens_outer_pupil_radius: %f", lens_outer_pupil_radius);
+                AiMsgInfo("[LENTIL CAMERA] lens_inner_pupil_radius: %f", lens_inner_pupil_radius);
+                AiMsgInfo("[LENTIL CAMERA] lens_length: %f", lens_length);
+                AiMsgInfo("[LENTIL CAMERA] lens_back_focal_length: %f", lens_back_focal_length);
+                AiMsgInfo("[LENTIL CAMERA] lens_effective_focal_length: %f", lens_effective_focal_length);
+                AiMsgInfo("[LENTIL CAMERA] lens_aperture_pos: %f", lens_aperture_pos);
+                AiMsgInfo("[LENTIL CAMERA] lens_aperture_housing_radius: %f", lens_aperture_housing_radius);
+                AiMsgInfo("[LENTIL CAMERA] lens_inner_pupil_curvature_radius: %f", lens_inner_pupil_curvature_radius);
+                AiMsgInfo("[LENTIL CAMERA] lens_outer_pupil_curvature_radius: %f", lens_outer_pupil_curvature_radius);
+                AiMsgInfo("[LENTIL CAMERA] lens_inner_pupil_geometry: %s", lens_inner_pupil_geometry.c_str());
+                AiMsgInfo("[LENTIL CAMERA] lens_outer_pupil_geometry: %s", lens_outer_pupil_geometry.c_str());
+                AiMsgInfo("[LENTIL CAMERA] lens_field_of_view: %f", lens_field_of_view);
+                AiMsgInfo("[LENTIL CAMERA] lens_aperture_radius_at_fstop: %f", lens_aperture_radius_at_fstop);
                 #endif
-                AiMsgInfo("[LENTIL CAMERA PO] --------------------------------------");
+                AiMsgInfo("[LENTIL CAMERA] --------------------------------------");
 
 
                 
-                AiMsgInfo("[LENTIL CAMERA PO] wavelength: %f nm", lambda);
+                AiMsgInfo("[LENTIL CAMERA] Wavelength: %f nm", lambda);
 
 
                 if (input_fstop == 0.0) {
@@ -1608,56 +1612,56 @@ private:
                     double calculated_aperture_radius = 0.0;
                     trace_backwards_for_fstop(input_fstop, calculated_fstop, calculated_aperture_radius);
                     
-                    AiMsgInfo("[LENTIL CAMERA PO] calculated fstop: %f", calculated_fstop);
-                    AiMsgInfo("[LENTIL CAMERA PO] calculated aperture radius: %f mm", calculated_aperture_radius);
+                    AiMsgInfo("[LENTIL CAMERA] Calculated f-stop: %f", calculated_fstop);
+                    AiMsgInfo("[LENTIL CAMERA] Calculated aperture radius: %f mm", calculated_aperture_radius);
                     
                     aperture_radius = std::min(lens_aperture_radius_at_fstop, calculated_aperture_radius);
                 }
 
-                AiMsgInfo("[LENTIL CAMERA PO] lens wide open f-stop: %f", lens_fstop);
-                AiMsgInfo("[LENTIL CAMERA PO] lens wide open aperture radius: %f mm", lens_aperture_radius_at_fstop);
-                AiMsgInfo("[LENTIL CAMERA PO] fstop-calculated aperture radius: %f mm", aperture_radius);
-                AiMsgInfo("[LENTIL CAMERA PO] --------------------------------------");
+                AiMsgInfo("[LENTIL CAMERA] Lens wide open f-stop: %f", lens_fstop);
+                AiMsgInfo("[LENTIL CAMERA] Lens wide open aperture radius: %f mm", lens_aperture_radius_at_fstop);
+                AiMsgInfo("[LENTIL CAMERA] F-stop-calculated aperture radius: %f mm", aperture_radius);
+                AiMsgInfo("[LENTIL CAMERA] --------------------------------------");
 
 
-                AiMsgInfo("[LENTIL CAMERA PO] user supplied focus distance: %f mm", focus_distance);
+                AiMsgInfo("[LENTIL CAMERA] User-supplied focus distance: %f mm", focus_distance);
 
                 /*
-                AiMsgInfo("[LENTIL CAMERA PO] calculating sensor shift at focus distance:");
+                AiMsgInfo("[LENTIL CAMERA] calculating sensor shift at focus distance:");
                 sensor_shift = camera_set_focus(focus_distance, po);
-                AiMsgInfo("[LENTIL CAMERA PO] sensor_shift to focus at %f: %f", focus_distance, sensor_shift);
+                AiMsgInfo("[LENTIL CAMERA] sensor_shift to focus at %f: %f", focus_distance, sensor_shift);
                 */
 
                 // logartihmic focus search
                 double best_sensor_shift = logarithmic_focus_search(focus_distance);
-                AiMsgInfo("[LENTIL CAMERA PO] sensor_shift using logarithmic search: %f mm", best_sensor_shift);
+                AiMsgInfo("[LENTIL CAMERA] Sensor shift using logarithmic search: %f mm", best_sensor_shift);
                 sensor_shift = best_sensor_shift + extra_sensor_shift;
 
                 /*
                 // average guesses infinity focus search
                 double infinity_focus_sensor_shift = camera_set_focus(AI_BIG, po);
-                AiMsgInfo("[LENTIL CAMERA PO] sensor_shift [average guesses backwards light tracing] to focus at infinity: %f", infinity_focus_sensor_shift);
+                AiMsgInfo("[LENTIL CAMERA] sensor_shift [average guesses backwards light tracing] to focus at infinity: %f", infinity_focus_sensor_shift);
                 */
 
                 // logarithmic infinity focus search
                 double best_sensor_shift_infinity = logarithmic_focus_search(999999999.0);
-                AiMsgInfo("[LENTIL CAMERA PO] sensor_shift [logarithmic forward tracing] to focus at infinity: %f mm", best_sensor_shift_infinity);
+                AiMsgInfo("[LENTIL CAMERA] Sensor shift (logarithmic forward tracing) to focus at infinity: %f mm", best_sensor_shift_infinity);
                     
                 // bidirectional parallel infinity focus search
                 double infinity_focus_parallel_light_tracing = camera_set_focus_infinity();
-                AiMsgInfo("[LENTIL CAMERA PO] sensor_shift [parallel backwards light tracing] to focus at infinity: %f mm", infinity_focus_parallel_light_tracing);
+                AiMsgInfo("[LENTIL CAMERA] Sensor shift (parallel backwards light tracing) to focus at infinity: %f mm", infinity_focus_parallel_light_tracing);
 
                 // double check where y=0 intersection point is, should be the same as focus distance
                 double test_focus_distance = 0.0;
                 bool focus_test = trace_ray_focus_check(sensor_shift, test_focus_distance);
-                AiMsgInfo("[LENTIL CAMERA PO] focus test ray: %f mm", test_focus_distance);
+                AiMsgInfo("[LENTIL CAMERA] Focus test ray: %f mm", test_focus_distance);
                 if(!focus_test){
-                    AiMsgWarning("[LENTIL CAMERA PO] focus check failed. Either the lens system is not correct, or the sensor is placed at a wrong distance.");
+                    AiMsgWarning("[LENTIL CAMERA] Focus check failed. Either the lens system is not correct or the sensor is placed at the wrong distance.");
                 }
 
                 tan_fov = std::tan(lens_field_of_view / 2.0);
 
-                AiMsgInfo("[LENTIL CAMERA PO] --------------------------------------");
+                AiMsgInfo("[LENTIL CAMERA] --------------------------------------");
 
             } break;
             case ThinLens:
